249d69cd2f391be7f30b4d3e05fe7a61bb65295e
Arthur Lam <lam.arthur0311@gmail.com>
2019-01-14 19:25:14 +0000

    added user authentication

--- download_avatars.js
+++ download_avatars.js
@@ -1,14 +1,23 @@
 var request = require('request');
+var token = require('./secrets.js');
+
+console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  request.get('https://api.github.com/repos/' + repoOwner + '/' + repoName +'/contributors')
-         .on('error', function(err){
-          throw err;
-         });
+  var options = {
+    url: 'https://api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors',
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': 'token ' + token.GITHUB_TOKEN
+    }
+  };
 
+  request(options, function(err, res, body){
+    cb(err, body);
+  });
 }
 
-getRepoContributors("jquery", "jquery", function(err, result) {
+getRepoContributors('jquery', 'jquery', function(err, result) {
   console.log("Errors:", err);
-  console.log("Result:", result);
+  console.log("Result:", JSON.parse(result));
 });
